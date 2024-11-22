2ffdf7a5628e60f7a3cf1782050d590991fb2784
Jeeva Dharani <jeevaselvi1999@gmail.com>
2024-11-23 01:03:47 +0530

    Installed the dotenv and updated the env variables for DB

--- rental-library-backend/config/database.config.js
+++ rental-library-backend/config/database.config.js
@@ -1,12 +1,12 @@
+require('dotenv').config();
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: 'jeevadharani',
-  host:  'localhost',
-  database: 'postgres',
-  password: 'postgres',
+  user: process.env.DB_USER,
+  host:  process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   port: 5432,
 });
 
-console.log(pool);
 module.exports = pool;
