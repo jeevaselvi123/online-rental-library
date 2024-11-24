1d3d9bcf602eab2a64425b5ffb4d4fad8cc5530e
Jeeva Dharani <jeevaselvi1999@gmail.com>
2024-11-24 14:30:05 +0530

    Updated max limit for Db

--- rental-library-backend/config/database.config.js
+++ rental-library-backend/config/database.config.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 
+console.log(process.env.DB_USER, process.env.DB_HOST, process.env.DB_DATABASE);
 const pool = new Pool({
   user: process.env.DB_USER,
   host:  process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
   port: 5432,
+  max: 10,
 });
 
 module.exports = pool;
